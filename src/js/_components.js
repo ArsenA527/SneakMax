aaf5d9f00ebe77aef7df09441092d95929702fa0
Arsen <arsen.avetisyan.2000@mail.ru>
2022-03-11 16:52:21 +0300

    first commit

---
+++ src/js/_components.js
@@ -0,0 +1,8 @@
+import './components/burger.js';
+import './components/cart.js';
+import './components/faq.js';
+import './components/map.js';
+import './components/range-slider.js';
+// import './components/validate.js';
+// import './components/products.js';
+import './components/quiz.js';
---
+++ src/js/components/map.js
@@ -0,0 +1,9 @@
+ymaps.ready(init);
+
+function init() {
+  const myMap = new ymaps.Map("map", {
+    center: [55.76, 37.64],
+    zoom: 14,
+    controls: ['geolocationControl', 'zoomControl']
+  });
+}
